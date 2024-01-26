d03d93a9c3b70050ced79b23c9a64f7a563e12ff
YeongJae0114 <yjl8628@naver.com>
2024-01-26 16:14:21 +0900

    :white_check_mark: MessageCodesResolver를 사용한 테스트 코드

---
+++ src/test/java/hello/itemservice/validation/MessageCodeResolverTest.java
@@ -0,0 +1,28 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+public class MessageCodeResolverTest {
+   MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+   @Test
+    void messageCodeResolverObject(){
+       String[] massageCode = codesResolver.resolveMessageCodes("required","item");
+       Assertions.assertThat(massageCode).containsExactly("required.item","required");
+   }
+
+    @Test
+    void messageCodeResolverField(){
+        String[] massageCode = codesResolver.resolveMessageCodes("required","item", "itemName", String.class);
+        Assertions.assertThat(massageCode).containsExactly(
+                "required.item.itemName"
+                ,"required.itemName"
+                ,"required.java.lang.String"
+                ,"required");
+    }
+
+
+}
