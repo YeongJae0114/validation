a9f9ae1a0d242057f561929f5b29ed0eea551ac5
YeongJae0114 <yjl8628@naver.com>
2024-01-26 16:51:59 +0900

    :sparkles: Validator 분리
    검증 로직을 새로운 클래스로 관리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -22,6 +22,8 @@ import java.util.List;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    // 검증코드 생성자 자동주입
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -115,8 +117,13 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            model.addAttribute("errers={}", bindingResult);
+            return "validation/v2/addForm";
+        }
         //검증 오류 결과를 보관은 BindingResult bindingResult
 
         //검증 로직
@@ -137,19 +144,28 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
-
-        //검증에 실패하면 다시 입력 폼으로
-        if(bindingResult.hasErrors()){
-            model.addAttribute("errers={}", bindingResult);
-            return "validation/v2/addForm";
-        }
-
         // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        itemValidator.validate(item, bindingResult);
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
 
 
     @GetMapping("/{itemId}/edit")
